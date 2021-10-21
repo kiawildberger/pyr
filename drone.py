887855cc8a53e03affff14ad80f9def5af8191a4
kiawildberger <kiawildberger@gmail.com>
2021-10-21 14:43:39 -0700

    update drone.py to assignment

--- drone.py
+++ drone.py
@@ -1,6 +1,11 @@
 from djitellopy import Tello
+feet = 30
+
 tello = Tello()
 tello.connect()
 tello.takeoff()
-tello.move_forward(100) # centimeters
+tello.move_forward(10*feet)
+tello.move_left(10*feet)
+tello.move_back(10*feet)
+tello.move_right(10*feet)
 tello.land()
