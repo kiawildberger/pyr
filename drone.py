5d30b0e0aec42e627c7482fdae092ab4b6e7e658
kiawildberger <kiawildberger@gmail.com>
2021-10-28 14:40:57 -0700

    rewrite with send_rc_control

--- drone.py
+++ drone.py
@@ -1,12 +1,17 @@
 from djitellopy import Tello
+from time import sleep
 feet = 30
+fps = 2 # feet per second
 
 tello = Tello()
 tello.connect()
 tello.takeoff()
-tello.move_forward(10*feet)
-tello.move_left(10*feet)
-tello.move_back(10*feet)
-tello.move_right(10*feet)
-tello.flip("b")
+tello.send_rc_control(0, 50, 0, 0)
+sleep(10/fps)
+tello.send_rc_control(-50, 0, 0, 0)
+sleep(10/fps)
+tello.send_rc_control(0, -50, 0, 0)
+sleep(10/fps)
+tello.send_rc_control(50, 0, 0, 0)
+
 tello.land()
