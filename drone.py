8da5276b8da225086a87f539e16ae6caeff1d4a3
kiawildberger <kiawildberger@gmail.com>
2021-11-02 13:47:10 -0700

    wacky math\!\?

--- drone.py
+++ drone.py
@@ -2,14 +2,15 @@ from djitellopy import Tello
 from time import sleep
 feet = 30
 speed = 0.5 # percentage of max
-fps = (112/30.4)*speed # feet per second, at speed
+fps = (112/30.4)*(1/speed) # feet per second, at speed
+vfps = (26/30.4)*(1/speed)
 
 tello = Tello()
 tello.connect()
 tello.takeoff()
 
 tello.send_rc_control(0, 0, speed*100, 0) # scoot another 3ft up
-sleep(3/fps)
+sleep(3/vfps)
 
 tello.send_rc_control(0, speed*100, 0, 0)
 sleep(10/fps)
