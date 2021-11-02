a0dfa7e1357aac7c9742874533c2119ff8d8f7f8
kiawildberger <kiawildberger@gmail.com>
2021-11-02 13:44:48 -0700

    move up after takeoff

--- drone.py
+++ drone.py
@@ -7,6 +7,10 @@ fps = (112/30.4)*speed # feet per second, at speed
 tello = Tello()
 tello.connect()
 tello.takeoff()
+
+tello.send_rc_control(0, 0, speed*100, 0) # scoot another 3ft up
+sleep(3/fps)
+
 tello.send_rc_control(0, speed*100, 0, 0)
 sleep(10/fps)
 tello.send_rc_control(-speed*100, 0, 0, 0)
