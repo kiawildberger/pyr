adf222dc67dbbad179f4a78a4d228e128e4d9fac
kiawildberger <kiawildberger@gmail.com>
2021-11-02 13:36:04 -0700

    update again with sendrc

--- drone.py
+++ drone.py
@@ -1,17 +1,18 @@
 from djitellopy import Tello
 from time import sleep
 feet = 30
-fps = 2 # feet per second
+speed = 0.5 # percentage of max
+fps = (112/30.4)*speed # feet per second
 
 tello = Tello()
 tello.connect()
 tello.takeoff()
-tello.send_rc_control(0, 50, 0, 0)
+tello.send_rc_control(0, speed*100, 0, 0)
 sleep(10/fps)
-tello.send_rc_control(-50, 0, 0, 0)
+tello.send_rc_control(-speed*100, 0, 0, 0)
 sleep(10/fps)
-tello.send_rc_control(0, -50, 0, 0)
+tello.send_rc_control(0, -speed*100, 0, 0)
 sleep(10/fps)
-tello.send_rc_control(50, 0, 0, 0)
+tello.send_rc_control(speed*100, 0, 0, 0)
 
 tello.land()
